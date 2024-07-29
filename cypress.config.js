6275f759e5f0b959c18f8b698ffff39e302b805e
duran <cheshire.dd96@gmail.com>
2024-07-29 15:12:53 +0200

    Added cypress

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: 'h2e6ee',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
